a7dceeaeaceaa15169f28e5d2988f8062e3b5259
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-01-28 14:54:52 -0800

    [sign binary] Add arg parser with option to set AWS profile

--- harmony-signing/sign_binary.py
+++ harmony-signing/sign_binary.py
@@ -1,12 +1,10 @@
-import aws_encryption_sdk
+import argparse
+
 import boto3
-import sys
 import hashlib
 import binascii
+from aws_encryption_sdk.exceptions import UnknownRegionError
 
-#key ID for harmony signing key, created by aws, using aws KMS 
-harmonySigningKeyId = "arn:aws:kms:us-west-2:656503231766:key/370d45bb-d629-45d7-8d1c-db0560895690"
-signingAlgorithm    = "RSASSA_PKCS1_V1_5_SHA_256"
 
 def region_from_key_id(signingKeyId, default_region=None):
     try:
@@ -20,36 +18,49 @@ def region_from_key_id(signingKeyId, default_region=None):
     return region_name
 
 
-def sign_harmony_file(inputFile, sigFile, keyId = harmonySigningKeyId):
+def sign_harmony_file(inputFile, sigFile, signingAlgorithm, keyId):
     region_name = region_from_key_id(keyId)
-    kms_client = boto3.client('kms', region_name)
+    kms_client = boto3.client('kms', region_name=region_name)
 
-    signature = ''
     with open(inputFile, "rb") as f:
         data = f.read()
         digest = hashlib.sha256(data).digest()
 
         print("File Name   : ", inputFile)
         print("Digest      : ", binascii.hexlify(digest))
-        print("Sign Key ID : ", harmonySigningKeyId)
+        print("Sign Key ID : ", keyId)
         print("Algorithm   : ", signingAlgorithm)
         print("Signature   : ", sigFile)
 
         response = kms_client.sign(
-            KeyId = keyId,
-            Message = digest,
-            MessageType = 'DIGEST',
+            KeyId=keyId,
+            Message=digest,
+            MessageType='DIGEST',
             SigningAlgorithm=signingAlgorithm
         )
 
         signature = response['Signature']
 
-    if signature != '':
-        with open(sigFile,"wb") as of:
+    if signature:
+        with open(sigFile, "wb") as of:
             of.write(signature)
 
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Sign binaries for harmony')
+    parser.add_argument('input_filepath', type=str, help="The binary file to be signed.")
+    parser.add_argument('output_sig_filepath', type=str, help="The output of the signature")
+    parser.add_argument('--sig_alg', type=str, help="The signing algorithm",
+                        default="RSASSA_PKCS1_V1_5_SHA_256")
+    parser.add_argument('--aws_key_id', type=str, help="The ARN signing key id.",
+                        default="arn:aws:kms:us-west-2:656503231766:key/370d45bb-d629-45d7-8d1c-db0560895690")
+    parser.add_argument('--aws_profile_name', type=str, help="The AWS profile with credentials",
+                        default="harmony-kms")
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: python3 sign_harmony_file [aws_key_id] [input_file] [output_signature]")
-    else:
-        sign_harmony_file(sys.argv[2], sys.argv[3], sys.argv[1])
+    args = parse_args()
+    boto3.setup_default_session(profile_name=args.aws_profile_name)
+    print("AWS profile : ", args.aws_profile_name)
+    sign_harmony_file(args.input_filepath, args.output_sig_filepath, args.sig_alg, args.aws_key_id)
